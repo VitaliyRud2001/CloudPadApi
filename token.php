305471bd7b48f7b676734e6df52252f5872b9c48
VitaliyRud2001 <vitaliy2001rudenko@gmail.com>
2020-01-22 12:34:10 +0200

    Added script for authorization and creating tokens

---
+++ token.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'DbConnection/mysql-connect.php';
+require_once 'DbConnection/mysql-info.php';
+require_once 'DbModels/userToken_model.php';
+
+$conn_obj = new mysql_connect($host,$user,$password,$database);
+$conn_obj->connectToDatabase();
+header('Content-type: application/json');
+if($_SERVER['REQUEST_METHOD']=='POST') {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $user_token = userToken_model::LogIn($email,$password,$conn_obj);
+        if($user_token!=null)
+        {
+            http_response_code(201);
+
+            $answer = array("token"=>$user_token->token);
+            exit(json_encode($answer));
+        }
+        else
+        {
+            http_response_code(401);
+            $answer = array("message"=>"incorrect password or login");
+            exit(json_encode($answer));
+        }
+    }
+
+
+
+}
+
+
+
+
+
+?>
