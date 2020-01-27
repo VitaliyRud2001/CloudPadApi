501267c6591a4ba9046d0d00c82f507df43ac3f2
VitaliyRud <vitaliy2001rudenko@gmail.com>
2020-01-27 11:11:46 +0200

    Script hor updating and creating pad notes

--- notes.php
+++ notes.php
@@ -2,11 +2,12 @@
 require_once 'DbConnection/mysql-connect.php';
 require_once 'DbConnection/mysql-info.php';
 require_once 'DbModels/userToken_model.php';
-
+require_once 'DbModels/note_model.php';
 $conn_obj = new mysql_connect($host,$user,$password,$database);
 $conn_obj->connectToDatabase();
 header('Content-type: application/json');
 if($_SERVER['REQUEST_METHOD']=='POST') {
+  $_POST = json_decode(file_get_contents('php://input'), true);
     if (isset($_POST['token']) && isset($_POST['note'])) {
       $token = $_POST['token'];
       $note = $_POST['note'];
@@ -14,11 +15,23 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
       $user_token->token = $token;
       if($user_token->TokenExists($conn_obj))
       {
-        echo "Yes,token exsists";
+        $note_model = new note_model();
+        $note_model->user_id = $user_token->user_id;
+        $note_model->note = $note;
+        if($note_model->CreateNote($conn_obj))
+        {
+          http_response_code(201);
+          $answer = array("message"=>"successfully created nodt");
+          exit(json_encode($answer));
+        }else{
+          http_response_code(400);
+          $answer = array("message"=>"error while creating note");
+        }
       }
-      else
-      {
-        echo "No fucking way!";
+      else{
+      http_response_code(401);
+      $answer = array("message"=>"incorrect token");
+      exit(json_encode($answer));
       }
 
     }else{
@@ -27,6 +40,35 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
       exit(json_encode($answer));
     }
 
+
+
+}
+else if($_SERVER['REQUEST_METHOD']=='PUT') {
+ $_PUT = json_decode(file_get_contents('php://input'), true);
+ $token = $_PUT['token'];
+ $note_id = $_PUT['note_id'];
+ $new_note = $_PUT['new_note'];
+
+ $user_token = new userToken_model();
+ $user_token->token = $token;
+  if($user_token->TokenExists($conn_obj)){
+    $note_model = new note_model();
+    $note_model->note_id = $note_id;
+    if($note_model->UpdateNote($conn_obj,$new_note))
+    {
+        http_response_code(200);
+        $answer = array("message"=>"successfuly updated note");
+        exit(json_encode($answer));
+    }else{
+        http_response_code(400);
+        $answer = array("message"=>"error while updating note");
+        exit(json_encode($answer));
+    }
+  }else{
+      http_response_code(401);
+      $answer = array("message"=>"incorrect token");
+      exit(json_encode($answer));
+  }
 
 
 }
